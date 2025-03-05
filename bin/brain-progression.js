3d181e313d7ea9b33084086fb49bd28be74984bb
Alex Bezverchuk <jd-bugs@yandex.com>
2025-03-05 16:06:05 -0600

    add a new game brain-progression

---
+++ bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import runGame from '../src/index.js';
+import completeProgression from '../src/games/progression.js';
+
+runGame(completeProgression);
---
+++ src/games/progression.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+const completeProgression = () => {
+  console.log('What number is missing in the progression?');
+  const minSeqLength = 5;
+  const maxSeqLength = 10;
+  const seqLength = Math.round(Math.random() * (maxSeqLength - minSeqLength + 1))
+      + minSeqLength;
+
+  // Generate a sequence step
+  const generateSecStep = () => {
+    const seqStep = Math.floor(Math.random() * 5) + 1;
+    return seqStep;
+  };
+  const seqStep = generateSecStep();
+
+  // Initiate the sequence beginning
+  let initSeqNum = Math.floor(Math.random() * 100);
+  const seqArr = [initSeqNum];
+
+  // Create the sequence
+  for (let i = 1; i < seqLength; i += 1) {
+    initSeqNum += seqStep;
+    seqArr.push(initSeqNum);
+  }
+
+  // Generate the question to user
+  const missedNum = Math.floor(Math.random() * seqLength); // Define a missing number
+  const correctAnswer = seqArr[missedNum]; // Memorize the missing number
+  seqArr[missedNum] = '...';
+  const questionExpression = seqArr.join(' ');
+  console.log(`Question: ${questionExpression}`);
+
+  return { question: questionExpression, answer: String(correctAnswer) };
+};
+
+export default completeProgression;
