c40d15b3b73d36dda5ead469200c8f1e2b22c4e5
Alex Bezverchuk <jd-bugs@yandex.com>
2025-03-11 21:53:26 -0500

    moved congratulations out of the function

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,35 +1,32 @@
 #!/usr/bin/env node
 const completeProgression = () => {
-  console.log('What number is missing in the progression?');
-  const minSeqLength = 5;
-  const maxSeqLength = 10;
-  const seqLength = Math.floor(Math.random() * (maxSeqLength - minSeqLength + 1)) + minSeqLength;
+  const description = 'What number is missing in the progression?';
 
-  // Generate a sequence step
-  const generateSecStep = () => {
+  const getQuestionAndAnswer = () => {
+    const minSeqLength = 5;
+    const maxSeqLength = 10;
+    const seqLength = Math.floor(Math.random() * (maxSeqLength - minSeqLength + 1)) + minSeqLength;
+
     const seqStep = Math.floor(Math.random() * 5) + 1;
-    return seqStep;
+
+    let initSeqNum = Math.floor(Math.random() * 100);
+    const seqArr = [initSeqNum];
+
+    for (let i = 1; i < seqLength; i += 1) {
+      initSeqNum += seqStep;
+      seqArr.push(initSeqNum);
+    }
+
+    const missedIndex = Math.floor(Math.random() * seqLength);
+    const correctAnswer = seqArr[missedIndex];
+    seqArr[missedIndex] = '..';
+
+    const questionExpression = seqArr.join(' ');
+
+    return { question: questionExpression, answer: String(correctAnswer) };
   };
-  const seqStep = generateSecStep();
 
-  // Initiate the sequence beginning
-  let initSeqNum = Math.floor(Math.random() * 100);
-  const seqArr = [initSeqNum];
-
-  // Create the sequence
-  for (let i = 1; i < seqLength; i += 1) {
-    initSeqNum += seqStep;
-    seqArr.push(initSeqNum);
-  }
-
-  // Generate the question to user
-  const missedNum = Math.floor(Math.random() * seqLength); // Define a missing number
-  const correctAnswer = seqArr[missedNum]; // Memorize the missing number
-  seqArr[missedNum] = '..';
-  const questionExpression = seqArr.join(' ');
-  console.log(`Question: ${questionExpression}`);
-
-  return { question: questionExpression, answer: String(correctAnswer) };
+  return { description, getQuestionAndAnswer };
 };
 
 export default completeProgression;
