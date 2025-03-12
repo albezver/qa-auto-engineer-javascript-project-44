16403c20e51b4c8153c850991d86779ddb89dc9a
Alex Bezverchuk <jd-bugs@yandex.com>
2025-03-11 21:54:16 -0500

    add a variable to define amount of game rounds

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 import readlineSync from 'readline-sync';
 import greetUser from './cli.js';
 
-const runGame = (gameRunner) => {
+const runGame = (game) => {
+  const { description, getQuestionAndAnswer } = game();
   const userName = greetUser();
+  console.log(description);
+  const roundsAmount = 3;
 
-  for (let i = 0; i < 3; i += 1) {
-    const { answer: correctAnswer } = gameRunner();
+  for (let i = 0; i < roundsAmount; i += 1) {
+    const { question, answer: correctAnswer } = getQuestionAndAnswer();
+
+    console.log(`Question: ${question}`);
 
     const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
 
     if (userAnswer === correctAnswer) {
       console.log('Correct!');
     } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
+      );
       console.log(`Let's try again, ${userName}!`);
-      break;
-    }
-
-    if (i === 2) {
-      console.log(`Congratulations, ${userName}!`);
+      return;
     }
   }
+
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default runGame;
